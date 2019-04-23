719e3413a16442ce653efefd7f27615579c4c210
Arne Küderle <a.kuederle@gmail.com>
2019-04-23 16:50:27 +0200

    Added up todate test data for regression test

--- tests/test_io.py
+++ tests/test_io.py
@@ -10,49 +10,53 @@ TEST_DATA = HERE / 'test_data'
 
 
 def test_load_simple():
-    path = TEST_DATA / 'simple_test_data.bin'
+    path = TEST_DATA / 'simple_synced_master.bin'
     dataset = Dataset(path=path)
 
     # Toplevel Stuff
     assert dataset.path == path
     assert dataset.imu_is_calibrated is False
     assert isinstance(dataset.acc, Datastream)
-    assert isinstance(dataset.gyro, Datastream)
+    assert dataset.gyro is None
     assert dataset.baro is None
+    assert dataset.mag is None
     assert dataset.battery is None
     assert dataset.analog is None
-    assert dataset.ACTIVE_SENSORS == ('acc', 'gyro')
+    assert dataset.ppg is None
+    assert dataset.ecg is None
+    assert dataset.ACTIVE_SENSORS == ('acc',)
 
     # Header
     info = dataset.info
-    assert info.sample_size == 16
+    assert info.sample_size == 10
     assert info.acc_enabled is True
-    assert info.gyro_enabled is True
+    assert info.mag_enabled is False
+    assert info.gyro_enabled is False
     assert info.baro_enabled is False
     assert info.analog_enabled is False
     assert info.ecg_enabled is False
     assert info.ppg_enabled is False
     assert info.battery_enabled is False
-    assert info.sampling_rate_hz == 102.4
+    assert info.sampling_rate_hz == 204.8
     assert info.session_termination == 'BLE'
     assert info.acc_range_g == 16
     assert info.gyro_range_dps == 2000
-    assert info.unix_time_start == 1555598546
-    assert info.datetime_start == datetime.datetime(2019, 4, 18, 16, 42, 26)
-    assert info.unix_time_stop == 1555598569
-    assert info.datetime_stop == datetime.datetime(2019, 4, 18, 16, 42, 49)
-    assert info.duration_s == 23
-    assert info.num_samples == 2367
-    assert info.version_firmware == 'v0.11.5'
-    assert info.sync_role == 'disabled'
-    assert info.is_synchronised is False
-    assert info.sync_group == 0
-    assert info.sync_index_start == 0  # not synced
-    assert info.sync_index_stop == 0  # not synced
-    assert info.mac_address == 'a9:db:22:ac:f7:29'
-    assert info.sensor_id == 'f729'
-    assert info.sync_address == '19efbeadde'
-    assert info.sync_channel == 27
+    assert info.unix_time_start == 1556025376
+    assert info.datetime_start == datetime.datetime(2019, 4, 23, 15, 16, 16)
+    assert info.unix_time_stop == 1556025420
+    assert info.datetime_stop == datetime.datetime(2019, 4, 23, 15, 17, 00)
+    assert info.duration_s == 44
+    assert info.num_samples == 9146
+    assert info.version_firmware == 'v0.12.0'
+    assert info.sync_role == 'master'
+    assert info.is_synchronised is True
+    assert info.sync_group == 9
+    assert info.sync_index_start == 0
+    assert info.sync_index_stop == 0
+    assert info.mac_address == '3a:x9:23:21:9e:82'
+    assert info.sensor_id == '9e82'
+    assert info.sync_address == '9f2be06f7c'
+    assert info.sync_channel == 43
 
     # System Info
     assert info.sensor_position == 'undefined'
