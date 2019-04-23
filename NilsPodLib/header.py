e68aa49b7a15c9c89f072cf5affd0484dab2fcf5
Arne Küderle <a.kuederle@gmail.com>
2019-04-23 12:55:33 +0200

    Adapted header to newet firmware version

--- NilsPodLib/header.py
+++ NilsPodLib/header.py
@@ -11,40 +11,56 @@ import datetime
 import numpy as np
 
 
+# TODO: Index start stop sync
+
+
 class Header:
     # default Session Header
+    acc_enabled: bool
     gyro_enabled: bool
-    acc_enabled: bool = True
-    pressure_enabled: bool
+    magnetometer_enabled: bool
     baro_enabled: bool
+    analog_enabled: bool
+    ecg_enabled: bool
+    ppg_enabled: bool
     battery_enabled: bool
+
+    sensor_position: str
+    session_termination: int
+    sample_size: int
 
     sampling_rate_hz: float
     acc_range_g: float
     gyro_range_dps: float
 
-    sync_role = 'disabled'
+    sync_role: str
     sync_distance_ms: float
-
-    sensor_position = 'undefined'
+    sync_group: int
+    sync_address: int
+    sync_channel: int
+    sync_index_start: int
+    sync_index_stop: int
 
     datetime_start: datetime.datetime
     datetime_stop: datetime.datetime
-    unix_time_start = 0
-    unix_time_stop = 0
+    unix_time_start: int
+    unix_time_stop: int
 
     version_firmware: str
-    packet_size: int
-    low_voltage_termination: bool
+    mac_address: str
 
-    meta_data = np.zeros(4)
+    system_settings = np.zeros(4)
     num_samples: int
 
     _SENSOR_FLAGS = {
-        'gyro_enabled': 0x01,
-        'pressure_enabled': 0x02,
-        'baro_enabled': 0x04,
-        'battery_enabled': 0x08
+        'acc_enabled': 0x01,
+        'gyro_enabled': 0x02,
+        'magnetometer_enabled': 0x04,
+        'baro_enabled': 0x08,
+        'analog_enabled': 0x10,
+        'ecg_enabled': 0x20,
+        'ppg_enabled': 0x40,
+        'battery_enabled': 0x80
     }
 
     _SAMPLING_RATES = {
@@ -55,7 +71,15 @@ class Header:
         1: 1024.0
     }
 
+    _SESSION_TERMINATION = {
+        'no memory': 0x10,
+        'BLE': 0x20,
+        'dock': 0x40,
+        'low battery': 0x80
+    }
+
     _SYNC_ROLE = {
+        0: 'disabled',
         1: 'slave',
         2: 'master'
     }
@@ -67,45 +91,64 @@ class Header:
     }
 
     def __init__(self, header_packet=None):
-        if header_packet is not None:
-            self.packet_size = header_packet[0]
-            sensors = header_packet[1]
 
+        # Note that because the info packet already has the first byte (info size) removed, all byte numbers are
+        # shifted compared to the documentation
+        if header_packet is not None:
+            self.sample_size = header_packet[0]
+
+            sensors = header_packet[1]
             for para, val in self._SENSOR_FLAGS.items():
                 setattr(self, para, bool(sensors & val))
 
             self.sampling_rate_hz = self._SAMPLING_RATES[header_packet[2] & 0x0F]
             self.samplingTime_ms = (1.0 / self.sampling_rate_hz) * 1000.0
 
-            self.low_voltage_termination = bool(header_packet[2] & 0x80)
+            self.session_termination = next(
+                k for k, v in self._SESSION_TERMINATION.items() if bool(header_packet[3] & v) is True)
 
-            self.sync_role = self._SYNC_ROLE.get(header_packet[3], self.sync_role)
+            self.sync_role = self._SYNC_ROLE[header_packet[4]]
 
-            self.sync_distance_ms = header_packet[4] * 100.0
+            self.sync_distance_ms = header_packet[5] * 100.0
 
-            self.acc_range_g = header_packet[6]
+            self.sync_group = header_packet[6]
 
-            self.gyro_range_dps = header_packet[7] * 125
+            self.acc_range_g = header_packet[7]
 
-            self.sensor_position = self._SENSOR_POS.get(header_packet[8], self.sensor_position)
+            self.gyro_range_dps = header_packet[8] * 125
 
-            self.meta_data = header_packet[9:13]
+            # self.sensor_position = self._SENSOR_POS.get(header_packet[8], self.sensor_position)
 
-            packed_date_time = int(header_packet[13]) | (int(header_packet[14]) << 8) | (
-                        int(header_packet[15]) << 16) | (
-                                       int(header_packet[16]) << 24)
-            self.unix_time_start = packed_date_time
+            self.system_settings = header_packet[9:14]
+
+            self.unix_time_start = self._convert_little_endian(header_packet[14:18])
             self.datetime_start = datetime.datetime.fromtimestamp(self.unix_time_start)
 
-            packed_date_time = int(header_packet[17]) | (int(header_packet[18]) << 8) | (
-                        int(header_packet[19]) << 16) | (
-                                       int(header_packet[20]) << 24)
-            self.unix_time_stop = packed_date_time
+            self.unix_time_stop = self._convert_little_endian(header_packet[18:22])
             self.datetime_stop = datetime.datetime.fromtimestamp(self.unix_time_stop)
 
-            self.num_samples = int(header_packet[21]) | (int(header_packet[22]) << 8) | (
-                        int(header_packet[23]) << 16) | (
-                                      int(header_packet[24]) << 24)
+            self.num_samples = self._convert_little_endian(header_packet[22:26])
 
-            self.version_firmware = "V" + str(int(header_packet[-3])) + "." + str(int(header_packet[-2])) + "." + str(
-                int(header_packet[-1]))
+            self.sync_index_start = self._convert_little_endian(header_packet[26:30])
+            self.sync_index_stop = self._convert_little_endian(header_packet[30:34])
+
+            self.mac_address = ':'.join([hex(int(x))[-2:] for x in header_packet[34:40]][::-1])
+
+            self.sync_address = ''.join([hex(int(x))[-2:] for x in header_packet[40:45]][::-1])
+            self.sync_channel = header_packet[45]
+
+            self.version_firmware = 'v{}.{}.{}'.format(*(int(x) for x in header_packet[-3:]))
+
+    @property
+    def duration_s(self) -> int:
+        return self.unix_time_stop - self.unix_time_start
+
+    @property
+    def is_synchronised(self) -> bool:
+        return not self.sync_role == 'disabled'
+
+    @staticmethod
+    def _convert_little_endian(byte_list):
+        number = int(byte_list[0]) | (int(byte_list[1]) << 8) | (
+                int(byte_list[2]) << 16) | (int(byte_list[3]) << 24)
+        return number
---
+++ tests/test_io.py
@@ -0,0 +1,52 @@
+import datetime
+from pathlib import Path
+
+from NilsPodLib.datastream import Datastream
+from NilsPodLib.dataset import Dataset
+
+HERE = Path(__file__).parent
+TEST_DATA = HERE / 'test_data'
+
+
+def test_load_simple():
+    path = TEST_DATA / 'simple_test_data.bin'
+    dataset = Dataset(path=path)
+
+    # Toplevel Stuff
+    assert dataset.path == path
+    assert dataset.is_calibrated is False
+    assert isinstance(dataset.baro, Datastream)
+    assert isinstance(dataset.acc, Datastream)
+    assert isinstance(dataset.gyro, Datastream)
+    assert isinstance(dataset.battery, Datastream)
+    assert isinstance(dataset.pressure, Datastream)
+
+    # Header
+    info = dataset.info
+    assert info.sample_size == 16
+    assert info.acc_enabled is True
+    assert info.gyro_enabled is True
+    assert info.baro_enabled is False
+    assert info.analog_enabled is False
+    assert info.ecg_enabled is False
+    assert info.ppg_enabled is False
+    assert info.battery_enabled is False
+    assert info.sampling_rate_hz == 102.4
+    assert info.session_termination == 'BLE'
+    assert info.acc_range_g == 16
+    assert info.gyro_range_dps == 2000
+    assert info.unix_time_start == 1555598546
+    assert info.datetime_start == datetime.datetime(2019, 4, 18, 16, 42, 26)
+    assert info.unix_time_stop == 1555598569
+    assert info.datetime_stop == datetime.datetime(2019, 4, 18, 16, 42, 49)
+    assert info.duration_s == 23
+    assert info.num_samples == 2367
+    assert info.version_firmware == 'v0.11.5'
+    assert info.sync_role == 'disabled'
+    assert info.is_synchronised is False
+    assert info.sync_group == 0
+    assert info.sync_index_start == 0  # not synced
+    assert info.sync_index_stop == 0  # not synced
+    assert info.mac_address == 'a9:db:22:ac:f7:29'
+    assert info.sync_address == '19efbeadde'
+    assert info.sync_channel == 27
