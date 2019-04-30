3fd6989b682be11344d3650ac9ee75f4646a559c
Arne Küderle <a.kuederle@gmail.com>
2019-04-30 13:05:36 +0200

    Simplified regression tests for future updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,33 @@
+import json
 from pathlib import Path
 
 import pytest
 from NilsPodLib.dataset import Dataset
 
 HERE = Path(__file__).parent
-TEST_DATA = HERE / 'test_data/sample_session'
+TEST_SESSION_DATA = HERE / 'test_data/sample_session'
+TEST_REGRESSION_DATA = HERE / 'test_data/sample_session_regression'
 
 
 @pytest.fixture()
 def dataset_master_simple():
-    path = TEST_DATA / 'simple_synced_master.bin'
+    path = TEST_SESSION_DATA / 'NilsPodX-7FAD_20190430_0933.bin'
     return Dataset.from_bin_file(path=path), path
 
 
 @pytest.fixture()
 def dataset_slave_simple():
-    path = TEST_DATA / 'simple_synced_slave.bin'
+    path = TEST_SESSION_DATA / 'NilsPodX-922A_20190430_0933.bin'
     return Dataset.from_bin_file(path=path), path
+
+
+@pytest.fixture()
+def dataset_master_simple_json_header():
+    return json.load((TEST_REGRESSION_DATA / 'NilsPodX-7FAD_20190430_0933_header.json').open('r'))
+
+
+@pytest.fixture()
+def dataset_slave_simple_json_header():
+    return json.load((TEST_REGRESSION_DATA / 'NilsPodX-922A_20190430_0933_header.json').open('r'))
+
+
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,9 +1,11 @@
 import datetime
+import json
 from unittest.mock import patch
 
 import numpy as np
 
 from NilsPodLib.datastream import Datastream
+from tests.conftest import TEST_SESSION_DATA, TEST_REGRESSION_DATA
 
 
 class MockDate(datetime.datetime):
@@ -16,67 +18,54 @@ class MockDate(datetime.datetime):
 
 
 @patch('datetime.datetime', MockDate)
-def test_load_simple(dataset_master_simple):
+def test_load_simple(dataset_master_simple, dataset_master_simple_json_header):
     dataset, path = dataset_master_simple
 
     # Toplevel Stuff
     assert dataset.path == path
     assert isinstance(dataset.acc, Datastream)
     assert dataset.acc.is_calibrated is False
-    assert dataset.gyro is None
+    assert isinstance(dataset.gyro, Datastream)
+    assert dataset.gyro.is_calibrated is False
     assert dataset.baro is None
     assert dataset.mag is None
     assert dataset.battery is None
     assert dataset.analog is None
     assert dataset.ppg is None
     assert dataset.ecg is None
-    assert dataset.ACTIVE_SENSORS == ('acc',)
+    assert dataset.ACTIVE_SENSORS == ('acc', 'gyro')
+
+    ## Uncomment to update regression files
+    # with open(TEST_REGRESSION_DATA / (str(path.stem) + '_header.json'), 'w+') as f:
+    #     f.write(dataset.info.to_json())
 
     # Header
+    # Check all direct values
     info = dataset.info
-    assert info.sample_size == 10
-    assert info.enabled_sensors == ('acc',)
-    assert info.sampling_rate_hz == 204.8
-    assert info.session_termination == 'BLE'
-    assert info.acc_range_g == 16
-    assert info.gyro_range_dps == 2000
-    assert info.utc_start == 1556025376
-    assert info.utc_datetime_start == datetime.datetime(2019, 4, 23, 13, 16, 16)
-    assert info.datetime_start == datetime.datetime(2019, 4, 23, 15, 16, 16)
-    assert info.utc_stop == 1556025420
-    assert info.utc_datetime_stop == datetime.datetime(2019, 4, 23, 13, 17, 00)
-    assert info.datetime_stop == datetime.datetime(2019, 4, 23, 15, 17, 00)
-    assert info.duration_s == 44
-    assert info.num_samples == 9146
-    assert info.version_firmware == 'v0.12.0'
-    assert info.version_hardware == '00'
-    assert info.sync_role == 'master'
+    assert dataset_master_simple_json_header == json.loads(info.to_json())
+    assert info.utc_datetime_start == datetime.datetime(2019, 4, 30, 7, 33, 12)
+    assert info.datetime_start == datetime.datetime(2019, 4, 30, 9, 33, 12)
+    assert info.utc_datetime_stop == datetime.datetime(2019, 4, 30, 7, 33, 59)
+    assert info.datetime_stop == datetime.datetime(2019, 4, 30, 9, 33, 59)
     assert info.is_synchronised is True
-    assert info.sync_group == 9
+    assert info.has_position_info is False
+    assert info.sensor_id == '7fad'
+
     assert info.sync_index_start == 0
     assert info.sync_index_stop == 0
-    assert info.mac_address == '3a:x9:23:21:9e:82'
-    assert info.sensor_id == '9e82'
-    assert info.sync_address == '9f2be06f7c'
-    assert info.sync_channel == 43
-    assert info.custom_meta_data == (0, 0, 0)
-
-    # System Info
-    assert info.sensor_position == 'undefined'
-    assert info.has_position_info is False
-    assert info.dock_mode_enabled is False
-    assert info.motion_interrupt_enabled is False
-    assert np.array_equal(info.custom_meta_data, np.zeros(3))
 
 
-def test_sync_info(dataset_slave_simple):
+def test_sync_info(dataset_slave_simple, dataset_master_simple, dataset_slave_simple_json_header):
     dataset, path = dataset_slave_simple
-    info = dataset.info
+    master = dataset_master_simple[0]
 
-    assert info.sync_role == 'slave'
-    assert info.is_synchronised is True
-    assert info.sync_group == 9  # Should be same as master (see test above)
-    assert info.sync_index_start == 489
-    assert info.sync_index_stop == 6636
-    assert info.sync_address == '9f2be06f7c'  # Should be same as master (see test above)
-    assert info.sync_channel == 43  # Should be same as master (see test above)
+    ## Uncomment to update regression files
+    # with open(TEST_REGRESSION_DATA / (str(path.stem) + '_header.json'), 'w+') as f:
+    #     f.write(dataset.info.to_json())
+
+    info = dataset.info
+    assert dataset_slave_simple_json_header == json.loads(info.to_json())
+    assert info.sync_group == master.info.sync_group
+    assert info.sync_address == master.info.sync_address
+    assert info.sync_channel == master.info.sync_channel
+    assert info.sync_index_stop != 0
