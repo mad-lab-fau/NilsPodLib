3d613d43311fd42915ee6da41a0fe8a201f3c380
Arne Küderle <a.kuederle@gmail.com>
2019-05-07 16:28:06 +0200

    Added icons for page

--- docs/conf.py
+++ docs/conf.py
@@ -104,6 +104,13 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "logo_portabiles.svg"
+html_favicon = "favicon_portabiles.ico"
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
