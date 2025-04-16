fixed byte casting errors (overflow) that occurs with newer numpy versions

nilspodlib/legacy.py
@@ -244,7 +244,7 @@ def load_11_2(header: np.ndarray, data_bytes: np.ndarray) -> Tuple[np.ndarray, n
 
 def _convert_analog_uint8_to_uint16_18_0(data_bytes, header_bytes):
     """Convert the data format of analog channels of uint8 to uint16."""
-    old_sample_size = header_bytes[1]
+    old_sample_size = int(header_bytes[1])
     temp_enabled = header_bytes[2] & 0x80
 
     if len(data_bytes) % old_sample_size:

nilspodlib/utils.py
@@ -93,7 +93,7 @@ def get_header_and_data_bytes(path: path_t) -> Tuple[np.ndarray, np.ndarray]:
         data_bytes = np.fromfile(f, dtype=np.dtype("B"))
 
     header = bytearray(header)
-    header_bytes = np.asarray(struct.unpack(str(header_size) + "b", header[0:header_size]), dtype=np.uint8)
+    header_bytes = np.asarray(struct.unpack(str(header_size) + "B", header[0:header_size]), dtype=np.uint8)
 
     return header_bytes, data_bytes
 